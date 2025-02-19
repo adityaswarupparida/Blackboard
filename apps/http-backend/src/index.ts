f96997f1fa00e7c0f884df152e9c4c576d7ce773
aparida <asparida33@gmail.com>
2025-02-20 03:56:26 +0530

    Add express server

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,44 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+
+import { middleware } from "./middleware";
+import { JWT_SECRET } from "./config";
 
 const app = express();
 const PORT = 3001;
+app.use(express.json());
+
+app.post("/signup", (req, res) => {
+    const { username, password } = req.body;
+
+    res.json({
+        userId: 123
+    })
+})
+
+app.post("/signin", (req, res) => {
+    const { username, password } = req.body;
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        token
+    })
+})
+
+app.post("/create-room", middleware, (req, res) => {
+    // const userId = req.userId;
+
+    // Create a new room for Admin = userId
+    const room = 1;
+    
+    res.json({
+        room
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening at port ${PORT}`);
