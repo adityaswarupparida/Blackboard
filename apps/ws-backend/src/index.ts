2b2b1dcb1c7e616458ba3c46db727376322d665e
aparida <asparida33@gmail.com>
2025-02-19 21:29:08 +0530

    Structure the Monorepo

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,10 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (socket) => {
+    socket.on("message", (data) => {
+        socket.send("Connected to Websocket");
+    })
+})
+
