4ec4c26e5b7a56cad2d808ead704dd303da69826
aparida <asparida33@gmail.com>
2025-02-20 03:53:28 +0530

    Add express and ws server

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,22 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (socket) => {
+wss.on("connection", (socket, request) => {
+    // ws://localhost:8000?token=2b78coy78xbo8nnxo89
+    const url = request.url ?? "";
+    const queryParams = new URLSearchParams(url.split("?")[1]);
+    const token = queryParams.get("token") ?? "";
+
+    const decodedData = jwt.verify(token, JWT_SECRET);
+
+    if(!decodedData || !(decodedData as JwtPayload).userId) {
+        socket.close();
+        return;
+    }
+
     socket.on("message", (data) => {
         socket.send("Connected to Websocket");
     })
